807940ebf83b2b4461326531644b6039bb45428b
Samuel Balcells <sfaith3@users.noreply.github.com>
2023-04-12 13:18:50 +0200

    Fix auto language + Some improvements in index.js

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+const Languages = {
+  English: "en",
+  Spanish: "es"
+}
 
 document.querySelector(".languageButton").addEventListener("click", function () {
   let newLanguage = this.innerHTML;
   switch (newLanguage) {
     case "Español":
-      newLanguage = "es";
+      newLanguage = Languages.Spanish;
       break;
     default:
-      newLanguage = "en";
+      newLanguage = Languages.English;
   }
   localStorage.currLanguage = newLanguage;
 });
@@ -26,12 +30,20 @@ function checkLanguage() {
       localStorage.currLanguage = browserLanguage;
     }
 
-    if (localStorage.currLanguage == "es") {
+    if (localStorage.currLanguage != Languages.English) {
+      if (!isLanguageAvailable(localStorage.currLanguage)) return;
       location.replace(location.protocol + "//" + mainURL + localStorage.currLanguage);
     }
   }
 }
 
 function getLanguageURL(url) {
-  return url.slice(url.lastIndexOf("/") + 1);
+  return url.slice(url.indexOf("/") + 1, url.lastIndexOf("/"));
+}
+
+function isLanguageAvailable(currLanguage) {
+  for (let language in Languages) {
+    if (Languages[language] === currLanguage) return true;
+  }
+  return false;
 }
